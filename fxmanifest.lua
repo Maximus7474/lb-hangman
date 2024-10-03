cf36c9a5922712031da354c0f8f5a98fc622eb05
Maximus7474 <the.maximus.7474@gmail.com>
2024-10-03 20:21:25 +0200

    feat: release workflow

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,7 @@ game "gta5"
 title "LB Hangman"
 description "A simple hangman game for LB Phone"
 author "Maximus7474"
+version ""
 repository "https://github.com/Maximus7474/lb-hangman"
 
 shared_script "shared.lua"
